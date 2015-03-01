c2fa8ab5464c4596b3332a6f8f0baa081b5e057c
Hoyool <hyoon3141@gmail.com>
2015-03-01 22:09:27 +0900

    admin css

--- kewme/kewme/settings.py
+++ kewme/kewme/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__)) + os.sep
 
-
+STATIC_ROOT = BASE_DIR + 'static'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
